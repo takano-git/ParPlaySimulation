1e492ed9ebb7e3b60e7b315c45fae5511c4e3e6a
平野忠清 <d85nus03@gmail.com>
2021-04-19 22:38:09 +0900

    Category model追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_101731) do
+ActiveRecord::Schema.define(version: 2021_04_19_130300) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "category_id", null: false
