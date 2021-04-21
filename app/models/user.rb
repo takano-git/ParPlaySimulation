3da2fe486f5860788a8d7efbe25af3e9e89aac68
topazon <topazonzon@gmail.com>
2021-04-21 16:26:36 +0900

    コード整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,9 @@ class User < ApplicationRecord
 protected
 # SNSログイン
   def self.from_omniauth(access_token)
-    # byebug
     data = access_token.info
     user = User.where(email: data['email']).first
 
-    # Uncomment the section below if you want users to be created if they don't exist
     unless user
         user = User.create(
           name: data['name'],
