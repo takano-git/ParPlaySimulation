eb90a75bbc41cf564090c9cbbb29c5ec3c6cb4a4
toshihiro-mabuchi <56546558+toshihiro-mabuchi@users.noreply.github.com>
2021-04-25 18:46:26 +0900

    redirect url削除

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,8 +275,7 @@ Devise.setup do |config|
   config.omniauth :google_oauth2,
                    Rails.application.credentials.google[:client_id], 
                    Rails.application.credentials.google[:secret_key], 
-                   skip_jwt: true,
-                   redirect_uri: "http://localhost:3000/users/auth/google_oauth2/callback"
+                   skip_jwt: true
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
