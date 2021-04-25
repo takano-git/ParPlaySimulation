d0572808268d4bbb1524b3afc8283487cc322d51
平野忠清 <d85nus03@gmail.com>
2021-04-25 12:17:05 +0900

    Course model追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_22_141503) do
+ActiveRecord::Schema.define(version: 2021_04_25_031412) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.integer "golfclub_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["golfclub_id"], name: "index_courses_on_golfclub_id"
   end
 
   create_table "golfclubs", force: :cascade do |t|
