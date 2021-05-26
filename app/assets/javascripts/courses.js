a60d2e6321cbcde224ee141c8b6c7e3a7c3f1e52
Tadakiyo-Hirano <d85nus03@gmaul.com>
2021-05-27 00:10:07 +0900

    ホール番号切替え機能修正

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,5 +1,6 @@
-window.onload = function() {
-  const hole_btn = document.querySelector("#hole_btn");
+// window.onload = function() {
+document.addEventListener("DOMContentLoaded", function () {
+  
   const hole_1 = document.querySelector("#hole_number_field_1");
   const hole_2 = document.querySelector("#hole_number_field_2");
   const hole_3 = document.querySelector("#hole_number_field_3");
@@ -18,31 +19,30 @@ window.onload = function() {
   const hole_16 = document.querySelector("#hole_number_field_16");
   const hole_17 = document.querySelector("#hole_number_field_17");
   const hole_18 = document.querySelector("#hole_number_field_18");
+  const hole_btn = document.querySelector("#hole_btn");
+  
 
-  document.querySelector("#hole_btn").onclick =
-  function () {
-    if (hole_btn.value === 'OUT') {
-      hole_btn.value = 'IN';
-      hole_1.value = 10;
-      hole_2.value = 11;
-      hole_3.value = 12;
-      hole_4.value = 13;
-      hole_5.value = 14;
-      hole_6.value = 15;
-      hole_7.value = 16;
-      hole_8.value = 17;
-      hole_9.value = 18;
+  $(document).on("click","[id^=hole_btn]",function() {
+    if (document.querySelector("#hole_number_field_1").value == 1) {
+      document.querySelector("#hole_number_field_1").value = 10;
+      document.querySelector("#hole_number_field_2").value = 11;
+      document.querySelector("#hole_number_field_3").value = 12;
+      document.querySelector("#hole_number_field_4").value = 13;
+      document.querySelector("#hole_number_field_5").value = 14;
+      document.querySelector("#hole_number_field_6").value = 15;
+      document.querySelector("#hole_number_field_7").value = 16;
+      document.querySelector("#hole_number_field_8").value = 17;
+      document.querySelector("#hole_number_field_9").value = 18;
     } else {
-      hole_btn.value = 'OUT';
-      hole_1.value = 1;
-      hole_2.value = 2;
-      hole_3.value = 3;
-      hole_4.value = 4;
-      hole_5.value = 5;
-      hole_6.value = 6;
-      hole_7.value = 7;
-      hole_8.value = 8;
-      hole_9.value = 9;
+      document.querySelector("#hole_number_field_1").value = 1;
+      document.querySelector("#hole_number_field_2").value = 2;
+      document.querySelector("#hole_number_field_3").value = 3;
+      document.querySelector("#hole_number_field_4").value = 4;
+      document.querySelector("#hole_number_field_5").value = 5;
+      document.querySelector("#hole_number_field_6").value = 6;
+      document.querySelector("#hole_number_field_7").value = 7;
+      document.querySelector("#hole_number_field_8").value = 8;
+      document.querySelector("#hole_number_field_9").value = 9;
     }
-  }
-};
+  })
+});
