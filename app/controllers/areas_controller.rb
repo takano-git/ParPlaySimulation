9fc33a3ef8654ff8edcd12eed5d88637ae69e476
asami <asami@asamitakanonoMacBook-Pro.local>
2021-05-29 08:39:43 +0900

    ゴルフ場検索ボタンにないものは無効を追加

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -2,7 +2,20 @@ class AreasController < ApplicationController
   # before_action 管理者のみアクセス可能にする(あとで設定)
 
   def index
-    @areas = Area.all.order(:id).group_by(&:district)
+    @areas = Area.all.order(:id).group_by(&:district) # 13地区でグループ化された47都道府県
+
+    @golfclub = Golfclub.first
+    # @prefectures = Area.all # 47都道府県のインスタンス
+    # # golfclub_of_prefecture = [[prefecture, ゴルフ場の数],[prefecture, ゴルフ場の数],[prefecture, ゴルフ場の数]]
+    # golfclubs_per_prefecture = [] #[[prefecture, golfclubs],[prefecture, golfclubs], ...]
+
+    # @prefectures.values.each do |prefecture|
+    #   golfclubs = Golfclub.where(area_id: id) #その件に登録されているゴルフ場のインスタンスを取得 []
+    #   golfclubs_per_prefecture = [].push([prefecture, golfclubs]) #["北海道" ,[]]
+    # end
+
+    # @golfclubs_per_prefecture = golfclubs_per_prefecture
+
   end
 
   def show
