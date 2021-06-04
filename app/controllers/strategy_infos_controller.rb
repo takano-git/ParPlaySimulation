bfe963b38016ff132e9ad376f3d9923a30aae92a
topazon <topazonzon@gmail.com>
2021-06-05 01:11:01 +0900

    RBG,href情報書き換え実装

--- app/controllers/strategy_infos_controller.rb
+++ app/controllers/strategy_infos_controller.rb
@@ -2,24 +2,32 @@ class StrategyInfosController < ApplicationController
 
   def index
     @golfclub = Golfclub.find(params[:golfclub_id])
+    @golfclub_id = @golfclub.id
     @courses = Course.where(golfclub_id: params[:golfclub_id])
+    @course_id = @courses.first.id
     @holes = Hole.where(
       golfclub_id: params[:golfclub_id], course_id: @courses.first.id
     )
-    @hole = Hole.where(golfclub_id: params[:golfclub_id]).first
+    @hole = @holes.first
     @strategy_info = StrategyInfo.where(golfclub_id: params[:golfclub_id]).first
     # byebug
   end
 
   def hole
-    # byebug
+    @golfclub_id = params[:golfclub_id]
+    @course_id = Course.find(params[:course_id]).id
     @holes = Hole.where(
       golfclub_id: params[:golfclub_id], course_id: params[:course_id]
     )
+    @hole = @holes.first
     @strategy_info = StrategyInfo.where(
       golfclub_id: params[:golfclub_id], course_id: params[:course_id]
     ).first
     # byebug
+  end
+
+  def location
+    @location_name = parames[:location_name]
   end
   
   def main 
