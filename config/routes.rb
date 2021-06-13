988dd1d860d52e402447cc1bf3240ec1a48426a5
Tadakiyo-Hirano <d85nus03@gmaul.com>
2021-06-13 19:29:41 +0900

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
       omniauth_callbacks: "users/omniauth_callbacks"
   }
 
-  resources :users, only: %i(show) do
+  resources :users do
     resources :posts
   end
 
