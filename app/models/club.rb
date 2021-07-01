e2667bcb26d48b6a2e777624ce67822c61a5fd98
asami <asami@asamitakanonoMacBook-Pro.local>
2021-07-01 22:51:53 +0900

    バリデーションを追加

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,4 +2,6 @@ class Club < ApplicationRecord
   belongs_to :user
 
   validates :yarn_count_string, :largo, :weight, presence: true
+  VALID_CLUB_REGEX = /\A[A-Z]+\z/
+  validates :yarn_count_string, format: { with: VALID_CLUB_REGEX }
 end
