027634db4f366aa9277cd313a449ee562d367951
asami <asami@asamitakanonoMacBook-Pro.local>
2021-07-02 16:18:22 +0900

    ゴルフクラブ登録フォームをセレクトボックスに変更

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,6 +2,11 @@ class Club < ApplicationRecord
   belongs_to :user
 
   validates :yarn_count_string, :largo, :weight, presence: true
-  VALID_CLUB_REGEX = /\A[A-Z]+\z/
-  validates :yarn_count_string, format: { with: VALID_CLUB_REGEX }
+  VALID_YARN_COUNT_STRING_REGEX = /\A[A-Z]+\z/
+  # 振動数は３桁の数字
+  VALID_FREQUENCY_REGEX = /\d+\d+\d/
+  
+  validates :yarn_count_string, format: { with: VALID_YARN_COUNT_STRING_REGEX }
+  validates :frequency, format: { with: VALID_FREQUENCY_REGEX }
+
 end
