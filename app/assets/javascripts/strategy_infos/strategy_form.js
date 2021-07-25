fd3e711921072e9ad55134b7cba4c84e2904fc59
topazon <topazonzon@gmail.com>
2021-07-25 11:02:17 +0900

    RBLボタン押下後のホールボタンURL書き換えの修正

--- app/assets/javascripts/strategy_infos/strategy_form.js
+++ app/assets/javascripts/strategy_infos/strategy_form.js
@@ -1,12 +1,5 @@
-
-// document.addEventListener("turbolinks:load", function() {
-
-
 $(document).bind('turbolinks:load ajaxComplete', function() {
-// -----
-// window.onload = function() {
-  // function() {
-  console.log("pinのscript")
+
   // ここからピンの移動
   const map_pin_target = document.getElementById("map_pin_target");
   const map_pin_point = document.getElementById("map_pin_point");
@@ -28,7 +21,7 @@ $(document).bind('turbolinks:load ajaxComplete', function() {
     // map取得
     let map = document.querySelector('.regedit_map');
     let map_rect = map.getBoundingClientRect();
-    let map_target_x = (x - map_rect.left)-width/2-3;
+    let map_target_x = (x - map_rect.left)+width/2-4;
     let map_target_y = (y - map_rect.top)-height/2-4;
     // ty-height/2-2とtx+width/2-6の値をhiddenのvalueに入れる
     map_pin_target.style.left = map_target_x + "px";
@@ -36,13 +29,6 @@ $(document).bind('turbolinks:load ajaxComplete', function() {
     // const
     document.getElementById('map_target_x').value = map_target_x;
     document.getElementById('map_target_y').value = map_target_y;
-    // --
-    // pin.onmouseup = function(event){
-      //   console.log("離れたぞ！");
-      //   // document.removeEventListener("mousemove",onMouseMove,true);
-      //   document.removeEventListener("mousemove", onMouseMove, false);
-      // }
-      // --
   }
   // マウスが離れたとき
   map_pin_target.onmouseup = function(event){
@@ -54,15 +40,8 @@ $(document).bind('turbolinks:load ajaxComplete', function() {
   map_pin_target.onmousedown = function(event){
     document.addEventListener("mousemove", pinMove, true)
   }
-  // pin.onmousedown = function(event){
-  //   document.addEventListener("mousemove", {
-    //     argumentPin: pin,
-    //     handleEvent: onMouseMove,
-    //   }, true);
-  // }
   
   
-  // 
   // 2.regedit_map_pin_point
   const MapPinPointMove = function onMouseMove(event){
     // let map_pin_point = document.getElementById("map_pin_point");
@@ -79,7 +58,7 @@ $(document).bind('turbolinks:load ajaxComplete', function() {
     // map取得
     let map = document.querySelector('.regedit_map');
     let map_point_rect = map.getBoundingClientRect();
-    let map_point_x = (x - map_point_rect.left)-width/2-3;
+    let map_point_x = (x - map_point_rect.left)+width/2-4;
     let map_point_y = (y - map_point_rect.top)-height/2-4;
     // ty-height/2-2とtx+width/2-6の値をhiddenのvalueに入れる
     map_pin_point.style.left = map_point_x + "px";
@@ -114,7 +93,7 @@ $(document).bind('turbolinks:load ajaxComplete', function() {
     // map取得
     let map = document.querySelector('.regedit_map');
     let map_shoot_rect = map.getBoundingClientRect();
-    let map_shoot_x = (x - map_shoot_rect.left)-width/2-3;
+    let map_shoot_x = (x - map_shoot_rect.left)+width/2-4;
     let map_shoot_y = (y - map_shoot_rect.top)-height/2-4;
     // ty-height/2-2とtx+width/2-6の値をhiddenのvalueに入れる
     map_pin_shoot.style.left = map_shoot_x + "px";
@@ -215,13 +194,5 @@ $(document).bind('turbolinks:load ajaxComplete', function() {
   }
   photo_pin_point.onmouseup = function(event){
     document.removeEventListener("mousemove",PhotoPinPointMove, true);
-  }  
-  
-  
-  
-  
-  
-// }
-// }
-// })
+  }
 });
