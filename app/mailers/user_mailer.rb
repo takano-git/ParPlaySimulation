4f4669fe89a003342fa7ae97fc4c08b0e7ba883b
asami <asami@asamitakanonoMacBook-Pro.local>
2021-07-26 19:31:59 +0900

    仮のメール送信機能追加

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+  default from: 'perplaysimulation@gmail.com'
+
+  # 登録クレジットカード有効期限間近のお知らせメール
+  def card_expired_mail
+    @user = params[:user]
+    @url  = 'https://perplaysimulation/login'
+    mail(to: @user.email, subject: 'ご登録クレジットカード有効期限に関するご案内')
+  end
+end
