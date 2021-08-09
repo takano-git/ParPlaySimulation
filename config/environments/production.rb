a7053b5d107dc65a2d88cbeeb46d2fe9b5d31594
asami <asami@asamitakanonoMacBook-Pro.local>
2021-08-09 19:57:31 +0900

    gメール設定をproduction.rbに追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,13 +76,15 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
       enable_starttls_auto: true,
       address: "smtp.gmail.com",
+      domain: 'gmail.com',
       port: 587,
       user_name: mail_address,
       password: password,
-      authentication: "plain"
+      authentication: "login"
   }
 
   config.action_mailer.perform_caching = false
