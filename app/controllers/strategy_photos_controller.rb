e3ce74c276833f8ccc404ac7e486f80614ca1e20
topazon <topazonzon@gmail.com>
2021-08-23 23:45:09 +0900

    写真一覧コース、ページネーション切り替え作成

--- app/controllers/strategy_photos_controller.rb
+++ app/controllers/strategy_photos_controller.rb
@@ -5,17 +5,32 @@ class StrategyPhotosController < ApplicationController
     @golfclub = Golfclub.find(params[:golfclub_id])
     @courses = Course.where(golfclub_id: params[:golfclub_id]).pluck(:id, :name)
     @course_options = Course.where(golfclub_id: params[:golfclub_id]).order(:id).map {
-      |c| [c.name, c.id, data: { children_path: switch_golfclub_strategy_infos_path(c.golfclub_id) }]
+      |c| [c.name, c.id, data: { children_path: golfclub_strategy_photos_path(c.golfclub_id) }]
     }
     @holes = Hole.where(golfclub_id: params[:golfclub_id]).pluck(:id, :hole_number)
     @strategy_infos = StrategyInfo.where(golfclub_id: params[:golfclub_id]).with_attached_photo
-    .order(:course_id, :hole_id, :location_name, :shot_id).page(params[:page]).per(10)
+    .order(:course_id, :hole_id, :location_name, :shot_id).page(params[:page]).per(36)
     # .page(params[:page]).per(36)
     user_ids = @strategy_infos.pluck(:user_id).sort.uniq
     @users = User.find(user_ids).pluck(:id, :nickname)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def course
+    @golfclub = Golfclub.find(params[:golfclub_id])
+    @courses = Course.where(golfclub_id: params[:golfclub_id]).pluck(:id, :name)
+    @course_options = Course.where(golfclub_id: params[:golfclub_id]).order(:id).map {
+      |c| [c.name, c.id, data: { children_path: golfclub_strategy_photos_path(c.golfclub_id) }]
+    }
+    @course_id = params[:course_id]
+    @holes = Hole.where(course_id: params[:course_id]).pluck(:id, :hole_number)
+    @strategy_infos = StrategyInfo.where(course_id: params[:course_id]).with_attached_photo
+    .order(:course_id, :hole_id, :location_name, :shot_id).page(params[:page]).per(36)
+    user_ids = @strategy_infos.pluck(:user_id).sort.uniq
+    @users = User.find(user_ids).pluck(:id, :nickname)
   end
 
   def destroy
